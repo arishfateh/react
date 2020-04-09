eeeca78dd4246d604c4aff9020b23717fc13afd5
arishfateh <arishfateh@gmail.com>
2020-04-09 19:59:17 +0500

    first commit

---
+++ arish.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class Arish extends Component {
+    render() {
+        return (
+            <div>
+                <div >
+                    <h1>hello from Arish!</h1>
+                    <h2>Here is a interesting image</h2>
+
+                </div>
+                <img src="https://i.ytimg.com/vi/7Nt7dQ2-3fE/hqdefault.jpg" alt="facts" />
+            </div>
+        );
+    }
+}
+
+export default Arish;
